ff494e52b98301fa64e74f86c9490e66bfa4b3d1
Nathaël Noguès <nathael.nogues@gridpocket.com>
2017-07-31 15:36:33 +0200

    Added branch Common

--- meter_gen/js/compile.sh
+++
@@ -1,9 +0,0 @@
-# @Author: Nathaël Noguès
-# @Date:   2017-07-31
-# @Last Modified by:   Nathaël Noguès
-# @Last Modified time: 2017-07-31
-
-SCRIPTPATH=$(dirname "$(readlink -f "$0")")
-cd $SCRIPTPATH
-
-npm install
