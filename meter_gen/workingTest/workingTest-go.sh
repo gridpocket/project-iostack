2905319f14b729b102d1111824683c4109fd861f
Nathaël Noguès <nathael.nogues@gridpocket.com>
2017-07-31 15:27:01 +0200

    Moved shared files out of branch

--- meter_gen/workingTest/workingTest-go.sh
+++ meter_gen/workingTest/workingTest-go.sh
@@ -3,10 +3,10 @@
 #  GridPocket Copyrights 2017
 # IOSTACK project www.iostack.eu
 #
-#  @Author: Nathaël Noguès
+# @Author: Nathaël Noguès
 # @Date:   2017-04-14
 # @Last Modified by:   Nathaël Noguès
-# @Last Modified time: 2017-07-28
+# @Last Modified time: 2017-07-31
 
 cd "`dirname \"$0\"`"
 sh workingTest.sh "../go/meter_gen"
